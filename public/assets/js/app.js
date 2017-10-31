43ce3012be2af26a31869affbe232eeaf5fc3381
Luca Spezzano <spezzanoluca@gmail.com>
2017-10-31 17:55:18 +0100

    done project1

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -42,35 +42,35 @@ new Vue({
     },
     computed: {
 
-        output: function() {
+        output: function () {
             console.log("output");
             return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
 
         }
 
-    },  
-/*
-    when counter change it runs
-*/
+    },
+    /*
+        when counter change it runs
+    */
     watch: {
-        counter: function(value) {
+        counter: function (value) {
             var vm = this;
-            setTimeout(function() {
-                vm.counter=0;
+            setTimeout(function () {
+                vm.counter = 0;
             }, 2000);
         }
     },
     methods: {
-     /*
-        increase: function () {
-            this.counter++;
+        /*
+           increase: function () {
+               this.counter++;
 
-        },
-        decrease: function () {
-            this.counter--;
-        },
-     */
-        result: function() {
+           },
+           decrease: function () {
+               this.counter--;
+           },
+        */
+        result: function () {
             console.log("result");
             return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
 
@@ -79,7 +79,7 @@ new Vue({
             this.x = event.clientX;
             this.y = event.clientY;
         },
-        alertMe: function() {
+        alertMe: function () {
             return alert("bau");
         }
     }
@@ -93,7 +93,7 @@ new Vue({
 
     },
     methods: {
-        alertMe: function() {
+        alertMe: function () {
             return alert("alert");
         }
     }
@@ -114,16 +114,16 @@ new Vue({
 
     },
     computed: {
-        result: function() {
+        result: function () {
             console.log("ciao")
             return this.value > 37 ? 'done' : 'not there yet';
         }
     },
     watch: {
-        value: function(value) {
+        value: function (value) {
             var vm = this;
-            setTimeout(function() {
-                vm.value=0;
+            setTimeout(function () {
+                vm.value = 0;
             }, 5000);
         }
     },
@@ -134,11 +134,136 @@ new Vue({
     el: '#exercise5',
     data: {
         attachBlue: false,
-         color: 'blue'
+        color: 'blue'
     },
     computed: {
-        isAttached: function() {
+        isAttached: function () {
             return this.attachBlue;
         }
     }
 });
+
+//-----------------------------------_---------------------------------
+// Section3
+
+new Vue({
+    el: '#exercise6',
+    data: {
+        show: true
+    }
+})
+
+new Vue({
+    el: '#exercise7',
+    data: {
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'luca', age: 24, color: 'red'},
+            {name: 'alex', age: 23, color: 'white'}
+        ]
+    }
+})
+
+new Vue({
+    el: '#project1',
+    data: {
+        started: false,
+        actions: [],
+        players: {
+            user: {
+                life: 100
+            },
+            monster: {
+                life: 100
+            }
+        }
+    },
+    methods: {
+        random: function (max, min) {
+            return Math.floor(Math.random() * max) + min;
+        },
+        checkWinner: function () {
+            if (this.players.user.life < 1) {
+              alert(" You lost! ");
+                this.giveUp();
+            }
+            else if (this.players.monster.life < 1) {
+               alert(" You Won! ");
+                this.giveUp();
+            }
+        },
+        doAttack: function () {
+            var damage = this.random(10, 1);
+            this.attack('user', damage);
+            damage = this.random(10, 1);
+            this.attack('monster', damage);
+            this.checkWinner();
+
+        },
+        doSpecialAttack: function () {
+            var damage = this.random(16, 3);
+            this.specialAttack('user', damage);
+            damage = this.random(16, 3);
+            this.specialAttack('monster', damage);
+            this.checkWinner();
+        },
+        doHeal: function () {
+            var heal = this.random(14, 5);
+            this.heal('user', heal);
+            heal = this.random(14, 5);
+            this.heal('monster', heal);
+        },
+
+        attack: function (player, damage) {
+            this.players[player].life -= damage;
+            this.log('attack', {
+                to: player,
+                damage: damage
+            });
+        },
+        specialAttack: function (player, damage) {
+            this.players[player].life -= damage;
+            this.log('specialAttack', {
+                to: player,
+                damage: damage
+            });
+        },
+        heal: function (player, heal) {
+            this.players[player].life += heal;
+            this.log('heal', {
+                to: player,
+                heal: heal
+            });
+        },
+
+        log: function (action, data) {
+            if (action === 'attack') {
+                this.actions.push({
+                    type: 'attack',
+                    to: data.to,
+                    damage: data.damage
+                });
+            } else if (action === 'heal') {
+                this.actions.push({
+                    type: 'heal',
+                    to: data.to,
+                    heal: data.heal
+                });
+            }
+            else if (action === 'specialAttack') {
+                this.actions.push({
+                    type: 'specialAttack',
+                    to: data.to,
+                    damage: data.damage
+                });
+            }
+        },
+
+        giveUp: function () {
+            this.players.user.life = 100;
+            this.players.monster.life = 100;
+            this.actions = [];
+            this.started = false;
+        }
+    }
+})
