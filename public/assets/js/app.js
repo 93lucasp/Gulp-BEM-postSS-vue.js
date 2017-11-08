38cd12ca100545d017af9ad23a68eb4fc3ff11a7
Luca Spezzano <spezzanoluca@gmail.com>
2017-11-08 12:56:52 +0100

    commented unuseless part

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,168 +1,168 @@
-new Vue({
-    el: '#app',
-    data: {
-        name: "Luca",
-        age: "24",
-        title: "hello world",
-        link: "http://google.com",
-        img: "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
-        linkFinished: "<a href='https://facebook.com'> facebook</a>"
-
-    },
-    methods: {
-        sayHello: function (event) {
-            this.title = "Hello"
-            return this.title;
-        }
-    }
-});
-new Vue({
-    el: '#exercise',
-    data: {
-        name: "Luca",
-        age: "24",
-        img: "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
-
-    },
-    methods: {
-        multiply: function () {
-            return Math.random();
-        }
-    }
-});
-
-new Vue({
-    el: '#counter',
-    data: {
-        counter: 0,
-        counter2: 0,
-        x: 0,
-        y: 0
-
-    },
-    computed: {
-
-        output: function () {
-            console.log("output");
-            return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
-
-        }
-
-    },
-    /*
-        when counter change it runs
-    */
-    watch: {
-        counter: function (value) {
-            var vm = this;
-            setTimeout(function () {
-                vm.counter = 0;
-            }, 2000);
-        }
-    },
-    methods: {
-        /*
-           increase: function () {
-               this.counter++;
-
-           },
-           decrease: function () {
-               this.counter--;
-           },
-        */
-        result: function () {
-            console.log("result");
-            return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
-
-        },
-        updateCoordinates: function (event) {
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        alertMe: function () {
-            return alert("bau");
-        }
-    }
-});
-
-
-new Vue({
-    el: '#exercise2',
-    data: {
-        value: ""
-
-    },
-    methods: {
-        alertMe: function () {
-            return alert("alert");
-        }
-    }
-});
-
-new Vue({
-    el: '#exercise3',
-    data: {
-        name: "luca"
-
-    }
-});
-
-new Vue({
-    el: '#exercise4',
-    data: {
-        value: 0,
-
-    },
-    computed: {
-        result: function () {
-            console.log("ciao")
-            return this.value > 37 ? 'done' : 'not there yet';
-        }
-    },
-    watch: {
-        value: function (value) {
-            var vm = this;
-            setTimeout(function () {
-                vm.value = 0;
-            }, 5000);
-        }
-    },
-
-});
-
-new Vue({
-    el: '#exercise5',
-    data: {
-        attachBlue: false,
-        color: 'blue'
-    },
-    computed: {
-        isAttached: function () {
-            return this.attachBlue;
-        }
-    }
-});
-
-//-----------------------------------_---------------------------------
-// Section3
-
-new Vue({
-    el: '#exercise6',
-    data: {
-        show: true
-    }
-})
-
-new Vue({
-    el: '#exercise7',
-    data: {
-        ingredients: ['meat', 'fruit', 'cookies'],
-        persons: [
-            {name: 'luca', age: 24, color: 'red'},
-            {name: 'alex', age: 23, color: 'white'}
-        ]
-    }
-})
+// new Vue({
+//     el: '#app',
+//     data: {
+//         name: "Luca",
+//         age: "24",
+//         title: "hello world",
+//         link: "http://google.com",
+//         img: "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
+//         linkFinished: "<a href='https://facebook.com'> facebook</a>"
+//
+//     },
+//     methods: {
+//         sayHello: function (event) {
+//             this.title = "Hello"
+//             return this.title;
+//         }
+//     }
+// });
+// new Vue({
+//     el: '#exercise',
+//     data: {
+//         name: "Luca",
+//         age: "24",
+//         img: "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
+//
+//     },
+//     methods: {
+//         multiply: function () {
+//             return Math.random();
+//         }
+//     }
+// });
+//
+// new Vue({
+//     el: '#counter',
+//     data: {
+//         counter: 0,
+//         counter2: 0,
+//         x: 0,
+//         y: 0
+//
+//     },
+//     computed: {
+//
+//         output: function () {
+//             console.log("output");
+//             return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
+//
+//         }
+//
+//     },
+//     /*
+//         when counter change it runs
+//     */
+//     watch: {
+//         counter: function (value) {
+//             var vm = this;
+//             setTimeout(function () {
+//                 vm.counter = 0;
+//             }, 2000);
+//         }
+//     },
+//     methods: {
+//         /*
+//            increase: function () {
+//                this.counter++;
+//
+//            },
+//            decrease: function () {
+//                this.counter--;
+//            },
+//         */
+//         result: function () {
+//             console.log("result");
+//             return this.counter > 10 ? 'greater then 10' : 'smaller then 10';
+//
+//         },
+//         updateCoordinates: function (event) {
+//             this.x = event.clientX;
+//             this.y = event.clientY;
+//         },
+//         alertMe: function () {
+//             return alert("bau");
+//         }
+//     }
+// });
+//
+//
+// new Vue({
+//     el: '#exercise2',
+//     data: {
+//         value: ""
+//
+//     },
+//     methods: {
+//         alertMe: function () {
+//             return alert("alert");
+//         }
+//     }
+// });
+//
+// new Vue({
+//     el: '#exercise3',
+//     data: {
+//         name: "luca"
+//
+//     }
+// });
+//
+// new Vue({
+//     el: '#exercise4',
+//     data: {
+//         value: 0,
+//
+//     },
+//     computed: {
+//         result: function () {
+//             console.log("ciao")
+//             return this.value > 37 ? 'done' : 'not there yet';
+//         }
+//     },
+//     watch: {
+//         value: function (value) {
+//             var vm = this;
+//             setTimeout(function () {
+//                 vm.value = 0;
+//             }, 5000);
+//         }
+//     },
+//
+// });
+//
+// new Vue({
+//     el: '#exercise5',
+//     data: {
+//         attachBlue: false,
+//         color: 'blue'
+//     },
+//     computed: {
+//         isAttached: function () {
+//             return this.attachBlue;
+//         }
+//     }
+// });
+//
+// //-----------------------------------_---------------------------------
+// // Section3
+//
+// new Vue({
+//     el: '#exercise6',
+//     data: {
+//         show: true
+//     }
+// })
+//
+// new Vue({
+//     el: '#exercise7',
+//     data: {
+//         ingredients: ['meat', 'fruit', 'cookies'],
+//         persons: [
+//             {name: 'luca', age: 24, color: 'red'},
+//             {name: 'alex', age: 23, color: 'white'}
+//         ]
+//     }
+// })
 
 new Vue({
     el: '#project1',
@@ -208,10 +208,8 @@ new Vue({
             this.checkWinner();
         },
         doHeal: function () {
-            var heal = this.random(14, 5);
+            var heal = this.random(10, 1);
             this.heal('user', heal);
-            heal = this.random(14, 5);
-            this.heal('monster', heal);
         },
 
         attack: function (player, damage) {
@@ -229,7 +227,10 @@ new Vue({
             });
         },
         heal: function (player, heal) {
-            this.players[player].life += heal;
+            if (this.players.user.life <= 1) {
+                this.players[player].life += heal;
+            }
+
             this.log('heal', {
                 to: player,
                 heal: heal
@@ -238,20 +239,20 @@ new Vue({
 
         log: function (action, data) {
             if (action === 'attack') {
-                this.actions.push({
+                this.actions.unshift({
                     type: 'attack',
                     to: data.to,
                     damage: data.damage
                 });
             } else if (action === 'heal') {
-                this.actions.push({
+                this.actions.unshift({
                     type: 'heal',
                     to: data.to,
                     heal: data.heal
                 });
             }
             else if (action === 'specialAttack') {
-                this.actions.push({
+                this.actions.unshift({
                     type: 'specialAttack',
                     to: data.to,
                     damage: data.damage
@@ -266,4 +267,30 @@ new Vue({
             this.started = false;
         }
     }
-})
+});
+// new Vue({
+//     el: '#exercise8',
+//     data: {
+//         title: 'The VueJS Instance',
+//         showParagraph: false
+//     },
+//     methods: {
+//         show: function() {
+//             this.showParagraph = true;
+//             this.updateTitle('The VueJS Instance (Updated)');
+//         },
+//         updateTitle: function(title) {
+//             this.title = title;
+//         }
+//     },
+//     computed: {
+//         lowercaseTitle: function() {
+//             return this.title.toLowerCase();
+//         }
+//     },
+//     watch: {
+//         title: function(value) {
+//             alert('Title changed, new value: ' + value);
+//         }
+//     }
+// });
